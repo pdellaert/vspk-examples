1ec2ee0158de9619322e4d662ca2eba9c1a55972
Philippe Dellaert <philippe@dellaert.org>
2019-05-28 18:43:50 +0200

    Adding tests for standard operations, async operations and bulk operations

---
+++ python/tests/test-vspk-bulk.py
@@ -0,0 +1,332 @@
+import pytest
+import random
+import time
+
+from vspk import v6_0 as vsdk
+
+transaction_status = {}
+
+def build_md_list(count=175):
+    md_list = list()
+    for num in range(0,count):
+        md_list.append(
+                vsdk.NUMetadata(name=num, blob=num)
+            )
+    return md_list
+
+def two_args_cb(a, b):
+    pass
+
+def bulk_async_cb(result, connection, errors):
+    transaction_status[connection.transaction_id] = {
+            'result': result,
+            'connection': connection,
+            'errors': errors
+        }
+
+def wait_for_transaction(trans_id, timeout=10):
+    wait_time = 0.0
+    while trans_id not in transaction_status.keys():
+        time.sleep(0.1)
+        wait_time += 0.1
+        if wait_time >= float(timeout):
+            return None, None, None
+    result = transaction_status[trans_id]['result']
+    connection = transaction_status[trans_id]['connection']
+    errors = transaction_status[trans_id]['errors']
+    return result, connection, errors
+
+def test_sync_bulk_root_object_create_children(nuage_connection, async_arg):
+    # Root object create_child
+    name_a = random.randint(100000,999999)
+    name_b = random.randint(100000,999999)
+    ent_a = vsdk.NUEnterprise(name=name_a)
+    ent_b = vsdk.NUEnterprise(name=name_b)
+    result, connection, errors = nuage_connection.user.create_children([ent_a, ent_b])
+    nuage_connection.user.bulk_delete([ent_a, ent_b], callback=bulk_async_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_single_create_children(nuage_connection, async_arg):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    result, connection, errors = ent.create_children(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_create_children(nuage_connection, async_arg):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    result, connection, errors = ent.create_children(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_single_save(nuage_connection, async_arg):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    result, connection, errors = ent.create_children(md_list)
+    for md in md_list:
+        md.name += 'u'
+    result, connection, errors = nuage_connection.user.bulk_save(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_save(nuage_connection, async_arg):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    result, connection, errors = ent.create_children(md_list)
+    for md in md_list:
+        md.name += 'u'
+    result, connection, errors = nuage_connection.user.bulk_save(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_single_delete(nuage_connection, async_arg):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    result, connection, errors = ent.create_children(md_list)
+    result, connection, errors = nuage_connection.user.bulk_delete(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_sync_bulk_object_delete(nuage_connection, async_arg):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    result, connection, errors = ent.create_children(md_list)
+    result, connection, errors = nuage_connection.user.bulk_delete(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_single_create_children(nuage_connection, async_arg):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    trans_id = ent.create_children(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_create_children(nuage_connection, async_arg):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    trans_id = ent.create_children(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_single_save(nuage_connection, async_arg):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    result, connection, errors = ent.create_children(md_list)
+    for md in md_list:
+        md.name += 'u'
+    trans_id = nuage_connection.user.bulk_save(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_save(nuage_connection, async_arg):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    result, connection, errors = ent.create_children(md_list)
+    for md in md_list:
+        md.name += 'u'
+    trans_id = nuage_connection.user.bulk_save(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_single_delete(nuage_connection, async_arg):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    result, connection, errors = ent.create_children(md_list)
+    trans_id = nuage_connection.user.bulk_delete(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 1
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_async_bulk_object_delete(nuage_connection, async_arg):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=175)
+    result, connection, errors = ent.create_children(md_list)
+    trans_id = nuage_connection.user.bulk_delete(md_list, callback=bulk_async_cb, **async_arg)
+    result, connection, errors = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 175
+    assert connection.response.status_code < 400
+    assert not errors
+
+def test_negative_bulk_object_create_children_without_list(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    with pytest.raises(TypeError):
+        assert ent.create_children(md_list[0])
+    ent.delete(callback=two_args_cb, **async_arg)
+
+def test_negative_bulk_object_create_children_illegal_child(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    md_list.append(vsdk.NUMetadata())
+    result, connection, errors = ent.create_children(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert not result[1].id
+    assert errors[0]['status'] >= 400
+
+def test_negative_bulk_object_create_children_duplicate_child(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent_list = [
+            vsdk.NUEnterprise(name=name),
+            vsdk.NUEnterprise(name=name)
+        ]
+    result, connection, errors = nuage_connection.user.create_children(ent_list)
+    for ent in ent_list:
+        if ent.id:
+            ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert not result[0].id or not result[1].id
+    assert errors[0]['status'] >= 400
+
+def test_negative_bulk_object_save_without_list(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=2)
+    ent.create_children(md_list)
+    md_list[1].blob = None
+    with pytest.raises(TypeError):
+        assert nuage_connection.user.bulk_save(md_list[1])
+    ent.delete(callback=two_args_cb, **async_arg)
+
+def test_negative_bulk_object_save_illegal_object(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=2)
+    ent.create_children(md_list)
+    md_list[1].blob = None
+    result, connection, errors = nuage_connection.user.bulk_save(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert errors[0]['status'] >= 400
+
+def test_negative_bulk_object_save_duplicate_object(nuage_connection, async_arg):
+    ent_list = [
+            vsdk.NUEnterprise(name=random.randint(100000,999999)),
+            vsdk.NUEnterprise(name=random.randint(100000,999999))
+        ]
+    nuage_connection.user.create_children(ent_list)
+    ent_list[1].name = ent_list[0].name = random.randint(100000,999999)
+    result, connection, errors = nuage_connection.user.bulk_save(ent_list)
+    nuage_connection.user.bulk_delete(ent_list, callback=bulk_async_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert errors[0]['status'] >= 400
+
+def test_negative_bulk_object_delete_without_list(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=2)
+    ent.create_children(md_list)
+    md_list[1].blob = None
+    with pytest.raises(TypeError):
+        assert nuage_connection.user.bulk_delete(md_list[1])
+    ent.delete(callback=two_args_cb, **async_arg)
+
+def test_negative_bulk_object_delete_illegal_object(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=2)
+    ent.create_children(md_list)
+    md_list[1].id = 'non-existing-id'
+    result, connection, errors = nuage_connection.user.bulk_delete(md_list)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert errors[0]['status'] >= 400
+
+def test_negative_bulk_object_delete_duplicate_object(nuage_connection, async_arg):
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    md_list = build_md_list(count=1)
+    ent.create_children(md_list)
+    result, connection, errors = nuage_connection.user.bulk_delete([md_list[0], md_list[0]])
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) == 2
+    assert connection.response.status_code < 400
+    assert len(errors) == 1
+    assert errors[0]['status'] >= 400
+
---
+++ python/tests/test-vspk-standard.py
@@ -0,0 +1,256 @@
+import pytest
+import random
+import time
+
+from vspk import v6_0 as vsdk
+
+# Object instantiate_child?
+
+def test_sync_object_create_child(nuage_connection):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    ent.delete()
+    assert ent.id
+    assert ent.name == '{0}'.format(name)
+
+def test_sync_fetcher_count(nuage_connection):
+    # Fetcher count
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    fetcher, parent, count = nuage_connection.user.enterprises.count(filter="name == '{0}'".format(name))
+    ent.delete()
+    assert count == 1
+
+def test_sync_fetcher_get_count(nuage_connection):
+    # Fetcher get_count
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    count = nuage_connection.user.enterprises.get_count(filter="name == '{0}'".format(name))
+    ent.delete()
+    assert count == 1
+
+def test_sync_fetcher_fetch(nuage_connection):
+    # Fetcher fetch
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    fetcher, parent, objs = nuage_connection.user.enterprises.fetch()
+    ent.delete()
+    assert len(objs) >= 1
+
+def test_sync_fetcher_get(nuage_connection):
+    # Fetcher get
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    objs = nuage_connection.user.enterprises.get()
+    ent.delete()
+    assert len(objs) >= 1
+
+def test_sync_fetcher_get_first(nuage_connection):
+    # Fetcher get_first
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    obj = nuage_connection.user.enterprises.get_first()
+    ent.delete()
+    assert type(obj) is vsdk.NUEnterprise
+
+def test_sync_object_fetch(nuage_connection):
+    # Object fetch
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    ent_new = vsdk.NUEnterprise(id=ent.id)
+    ent_new.fetch()
+    ent.delete()
+    assert ent_new.name == '{0}'.format(name)
+
+def test_sync_object_save(nuage_connection):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    new_name = random.randint(1000000,9999999)
+    ent.name = new_name
+    ent.save()
+    ent.fetch()
+    ent.delete()
+    assert ent.name == '{0}'.format(new_name)
+
+def test_sync_object_assign(nuage_connection):
+    # Object assign
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    usr = vsdk.NUUser(user_name="sync-user", password="sync-user", first_name="sync", last_name="user", email="sync@user.local")
+    ent.create_child(usr)
+    grp = ent.groups.get_first()
+    grp.assign([usr], vsdk.NUUser)
+    grp_users = grp.users.get()
+    usr.delete()
+    ent.delete()
+    assert len(grp_users) == 1
+
+def test_sync_object_delete(nuage_connection):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    ent.delete()
+    count = nuage_connection.user.enterprises.get_count(filter="name == '{0}'".format(name))
+    assert count == 0
+
+transaction_status = {}
+def two_args_cb(obj, conn):
+    transaction_status[conn.transaction_id] = {
+        'obj': obj,
+        'conn': conn
+    }
+
+def three_args_cb(fetcher, parent, result):
+    transaction_status[fetcher.transaction_id] = {
+        'fetcher': fetcher,
+        'parent': parent,
+        'result': result
+    }
+
+def wait_for_transaction(trans_id, timeout=5):
+    wait_time = 0.0
+    while trans_id not in transaction_status.keys():
+        time.sleep(0.1)
+        wait_time += 0.1
+        if wait_time >= float(timeout):
+            return None, None
+    obj = transaction_status[trans_id]['obj']
+    conn = transaction_status[trans_id]['conn']
+    return obj, conn
+
+def wait_for_fetcher_transaction(trans_id, timeout=5):
+    wait_time = 0.0
+    while trans_id not in transaction_status.keys():
+        time.sleep(0.1)
+        wait_time += 0.1
+        if wait_time >= float(timeout):
+            return None, None
+    fetcher = transaction_status[trans_id]['fetcher']
+    parent = transaction_status[trans_id]['parent']
+    result = transaction_status[trans_id]['result']
+    return fetcher, parent, result
+
+
+def test_async_object_create_child(nuage_connection, async_arg):
+    # Object create_child
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    trans_id = nuage_connection.user.create_child(ent, callback=two_args_cb, **async_arg)
+    obj, conn = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert conn.response.status_code < 400
+
+def test_async_fetcher_count(nuage_connection, async_arg):
+    # Fetcher count
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    trans_id = nuage_connection.user.enterprises.count(filter="name == '{0}'".format(name), callback=three_args_cb, **async_arg)
+    fetcher, parent, result = wait_for_fetcher_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert result == 1
+
+def test_async_fetcher_get_count(nuage_connection, async_arg):
+    # Fetcher get_count
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    count = nuage_connection.user.enterprises.get_count(filter="name == '{0}'".format(name))
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert count == 1
+
+def test_async_fetcher_fetch(nuage_connection, async_arg):
+    # Fetcher fetch
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    trans_id = nuage_connection.user.enterprises.fetch(callback=three_args_cb, **async_arg)
+    fetcher, parent, result = wait_for_fetcher_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(result) >= 1
+
+def test_async_fetcher_get(nuage_connection, async_arg):
+    # Fetcher get
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    objs = nuage_connection.user.enterprises.get()
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert len(objs) >= 1
+
+def test_async_fetcher_get_first(nuage_connection, async_arg):
+    # Fetcher get_first
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    obj = nuage_connection.user.enterprises.get_first()
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert type(obj) is vsdk.NUEnterprise
+
+def test_async_object_fetch(nuage_connection, async_arg):
+    # Object fetch
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    ent_new = vsdk.NUEnterprise(id=ent.id)
+    trans_id = ent_new.fetch(callback=two_args_cb, **async_arg)
+    obj, conn = wait_for_transaction(trans_id)
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert conn.response.status_code < 400
+    assert ent_new.name == '{0}'.format(name)
+
+def test_async_object_save(nuage_connection, async_arg):
+    # Object save
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    new_name = random.randint(1000000,9999999)
+    ent.name = new_name
+    trans_id = ent.save(callback=two_args_cb, **async_arg)
+    obj, conn = wait_for_transaction(trans_id)
+    ent.fetch()
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert conn.response.status_code < 400
+    assert type(trans_id) is str
+    assert ent.name == '{0}'.format(new_name)
+
+def test_async_object_assign(nuage_connection, async_arg):
+    # Object assign
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    usr = vsdk.NUUser(user_name="sync-user", password="sync-user", first_name="sync", last_name="user", email="sync@user.local")
+    ent.create_child(usr)
+    grp = ent.groups.get_first()
+    trans_id = grp.assign([usr], vsdk.NUUser, callback=two_args_cb, **async_arg)
+    obj, conn = wait_for_transaction(trans_id)
+    grp_users = grp.users.get()
+    usr.delete()
+    ent.delete(callback=two_args_cb, **async_arg)
+    assert conn.response.status_code < 400
+    assert len(grp_users) == 1
+
+def test_async_object_delete(nuage_connection, async_arg):
+    # Object delete
+    name = random.randint(100000,999999)
+    ent = vsdk.NUEnterprise(name=name)
+    nuage_connection.user.create_child(ent)
+    trans_id = ent.delete(callback=two_args_cb, **async_arg)
+    obj, conn = wait_for_transaction(trans_id)
+    count = nuage_connection.user.enterprises.get_count(filter="name == '{0}'".format(name))
+    assert conn.response.status_code < 400
+    assert type(trans_id) is str
+    assert count == 0
+
