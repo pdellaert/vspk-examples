ed07688f5be38ef5646d92567dd1c7ef7b0612a9
Philippe Dellaert <philippe@dellaert.org>
2019-05-28 18:43:50 +0200

    Adding tests for standard operations, async operations and bulk operations

---
+++ python/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import sys
+from vspk import v6_0 as vsdk
+
+@pytest.fixture(scope="session")
+def nuage_connection():
+    api_url = 'https://localhost:8444'
+    username = 'csproot'
+    password = 'csproot'
+    enterprise = 'csp'
+
+    nuage_connection = vsdk.NUVSDSession(api_url=api_url, username=username, password=password, enterprise=enterprise)
+    nuage_connection.start()
+    return nuage_connection
+
+@pytest.fixture(scope="session")
+def async_arg():
+    if sys.version_info >= (3,7):
+        async_arg = { 'as_async': True }
+    else: 
+        async_arg = { 'async': True }
+    return async_arg
+
